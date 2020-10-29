1c3f554b1b0ae76649cd25d9b94bd15a32c2addd
Joseph Diza <josephm.diza@gmail.com>
2020-10-28 21:15:32 -0700

    Add sdkman.sh rc startup script and init script

---
+++ .cfg/scripts/tstart/sdkman.sh
@@ -0,0 +1,5 @@
+#!/bin/bash/
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="/home/jmd/.sdkman"
+[[ -s "/home/jmd/.sdkman/bin/sdkman-init.sh" ]] && source "/home/jmd/.sdkman/bin/sdkman-init.sh"
