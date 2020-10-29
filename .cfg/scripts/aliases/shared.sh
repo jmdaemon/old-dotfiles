21071869953cb8ee12046bcba75995c46910ba2d
Joseph Diza <josephm.diza@gmail.com>
2020-10-28 21:28:39 -0700

    Add alias for ml.zsh

--- .cfg/scripts/aliases/shared.sh
+++ .cfg/scripts/aliases/shared.sh
@@ -46,6 +46,7 @@ alias gdev="cd $HOME/git/fastai_dev/"
 alias gdocs="cd $HOME/git/fastai_docs/"
 alias gcv="cd $HOME/git/course-v3"
 alias gfast="cd $HOME/git/fastprogress"
+alias mls="source ./cfg/scripts/ml.zsh"
 
 alias gts="cd $HOME/workspace/School/" 
 alias gtws="cd $HOME/workspace"
