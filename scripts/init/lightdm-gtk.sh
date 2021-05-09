96e4c641147d2fa24fc233658260080925d6165c
Joseph Diza <josephm.diza@gmail.com>
2021-05-08 20:00:36 -0700

    Remove redundant scripts and obsolete code

--- scripts/init/lightdm-gtk.sh
+++ scripts/init/lightdm-gtk.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-#THEME=Flat-Remix-GTK-Red-Darker
-#ICON=Flat-Remix-Red-Dark
-#FONT="Sans 13"
-#WALLPAPER=/usr/share/pixmaps/Firewatch.jpg
-#CLOCK="%a - %m/%d/%y - %H:%M - %I:%M%p"
-
-#xft.dpi = 220
-#xft-antialias = true
-#xft-dpi = 150
-#xft-rgba = rgb
-#xft-hintstyle = hintfull
-
 echo "
 [greeter]
 theme-name = Flat-Remix-GTK-Red-Darker
