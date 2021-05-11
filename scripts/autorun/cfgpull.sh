fa93c233d5b261e72293d7e7c2f621099a9ccdda
Joseph Diza <josephm.diza@gmail.com>
2021-05-10 22:19:48 -0700

    Remove obsolete cfgpull.sh

--- scripts/autorun/cfgpull.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Update to latest sys config
-shopt -s expand_aliases # Enable aliases
-alias cfg='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-BR=$(cfg branch --show-current)
-cfg fetch upstream $BR # Get newest changes
